37f7ff01debdf26203046e3b98230a37c6518f09
yutsukushi <ykushibi@gmail.com>
2019-07-24 20:48:38 -0400

    able to calculate differences between user input and friends already in server, as well as, getting the sum of the arrays

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,40 +17,51 @@ module.exports = function(app) {
     // console.log(request.answers);
 
     // for loop to turn every answer in array into an integer
-    var intArray = [];
-    var totalDiff = [];
+    var intArray = []; //integer array for user input
+    var totalDiff = [], smArr; //integer array for friends in storage
 
     // for every object in friend array,
     for (var f = 0; f < friends.length; f++) {
       // difference between each answers index integer to newFriend index answers
-          for (var i = 0; i < request.answers.length; i++) { 
-            //converts strings to integers per index and pushes into intArray
-            intArray.push(parseInt(request.answers[i]));
-            
-            totalDiff.push(Math.abs(friends[f].answers[i] - intArray[i]));
+        for (var i = 0; i < request.answers.length; i++) { 
+          //converts strings to integers per index and pushes into intArray
 
-            // if (totalDiff[i] >= 9) {
-            //   intArray = [];
-            //   totalDiff = [];
-            // }
-            
-            console.log(totalDiff);
-            
-          }
+          intArray.push(parseInt(request.answers[i]));
+          
+          totalDiff.push(Math.abs(friends[f].answers[i] - intArray[i]));
+          // when index hits 9, split to new array.
+          
         }
-    
-    //  console.log(friends[0].answers[0] - intArray[0]);
-    // collect the difference and add, if friends index has lowest difference
-    // post photo image in a modal with their name.
-    
-    console.log(intArray);
-    // console.log(totalDiff);
 
-    // calculate between friends array answers and new friend array answers
-    // for loop to do comparison calculation for each friend
+        if (totalDiff.length > 0) {
+
+          smArr = totalDiff.splice(0,10)
     
+          console.log(smArr);
     
+        }
+        
+    }
+
+    // collect the difference and add, 
+      
+      var totalNew = 0;
+      var totalFriends = 0;
+
+      for (var i in smArr) { 
+        totalNew += intArray[i]; 
+        totalFriends += smArr[i];
+      }
+  
+    console.log(totalNew);
+    console.log(totalFriends);
+    // post photo image in a modal with their name.
+
+    // if friends index has lowest difference
     // lowest totalDifference variable will be posted as best match
+    if (totalFriends >= totalNew) {
+
+    }
     // send best match photoImage as a modal
     res.json(request);
 
