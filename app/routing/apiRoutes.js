052325e37192b0214220dca111fae97b9c90fe61
yutsukushi <ykushibi@gmail.com>
2019-07-27 22:40:55 -0400

    Functioning modal with returning proper data according to users responses

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,3 @@
-// might need possibly for modal popup
 var friends = require("../data/friends.js");
 
 module.exports = function(app) {
@@ -6,60 +5,38 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
      res.json(friends);
      console.log(friends);
-    });
+  });
   
-  // Create New reservertaion - takes in JSON input
   app.post("/api/friends", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
 
-    var request = req.body;
+    var userInfo = req.body;
 
-    var bestMatch = [{
-      name: "",
-      photoImage: "",
-      totalDiff: 20
-    }]
-    // run a nested for loop of friends object
-    // userAnswers and currFrndAnswer
+    var bestMatch;
+    var currentLowestDifference;
 
-    // // for every object in friend array,
     for (var f = 0; f < friends.length; f++) {
 
       var totalDiff = 0;
-    //   // difference between each answers index integer to newFriend index answers
-        for (var i = 0; i < request.answers.length; i++) { 
-       //converts strings to integers per index and pushes into intArray
-        
-        // collect the difference and add, 
-      
-        totalDiff += (Math.abs(parseInt(friends[f].answers[i]) - parseInt(request.answers[i])));
+
+      for (var i = 0; i < userInfo.answers.length; i++) { 
+
+        totalDiff += (Math.abs(parseInt(friends[f].answers[i]) - parseInt(userInfo.answers[i])));
 
         console.log(totalDiff);
-
-        if (bestMatch.totalDiff >= totalDiff) {
-          friends[f].name = bestMatch.name;
-          friends[f].photoImage = bestMatch.photoImage;
-          totalDiff = bestMatch.totalDiff; //prints name
-          
-        }
-    
         console.log(friends[f].name);
 
       }
       
+      if (currentLowestDifference == null || totalDiff < currentLowestDifference) {
+        currentLowestDifference = totalDiff;
+        bestMatch = friends[f];
+      }
+  
     }
 
-    // // post photo image in a modal with their name.
-
-    // // if friends index has lowest difference
-    // // lowest totalDifference variable will be posted as best match
- 
-    // send best match photoImage as a modal
-    friends.push(request);
+    friends.push(userInfo);
 
     res.json(bestMatch);
-
     });
     
 }
