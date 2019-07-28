a104b6880b53bbea2e2c48e8f881eb8efa2c0c82
yutsukushi <ykushibi@gmail.com>
2019-07-28 13:37:45 -0400

    added comments to js file, included image responsiveness to modal, began form validation code to require name and iamgeURL

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,19 +14,21 @@ module.exports = function(app) {
     var bestMatch;
     var currentLowestDifference;
 
+    // loop through friend object array
     for (var f = 0; f < friends.length; f++) {
 
+      // setting totalDiff to 0, so it resets after each answer length
       var totalDiff = 0;
 
+      // loops through user answers length
       for (var i = 0; i < userInfo.answers.length; i++) { 
 
+        // equation to calculate absolute total difference between user and friend answers
         totalDiff += (Math.abs(parseInt(friends[f].answers[i]) - parseInt(userInfo.answers[i])));
-
-        console.log(totalDiff);
-        console.log(friends[f].name);
 
       }
       
+      // if statement to change variable values for bestMatch and currentLowestDifference to the values of a friend that is the least away from you
       if (currentLowestDifference == null || totalDiff < currentLowestDifference) {
         currentLowestDifference = totalDiff;
         bestMatch = friends[f];
@@ -34,8 +36,10 @@ module.exports = function(app) {
   
     }
 
+    // pushes user info to the friend object array
     friends.push(userInfo);
 
+    // sends bestMatch info to surveyHTML to populate modal
     res.json(bestMatch);
     });
     
